3bb8ea34d97b064eef4de5022cf37f3f35e37f68
Huang Junye <h.jun.ye@gmail.com>
2019-10-07 12:13:20 +0800

    try to add python path

--- blender/bloch_sphere.py
+++ blender/bloch_sphere.py
@@ -1,4 +1,15 @@
 import bpy
+import sys
+import os
+
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir )
+    print(sys.path)
+
+bpy.ops.object.select_all(action="SELECT")
+bpy.ops.object.delete(use_global=False)
+
 bpy.ops.mesh.primitive_ico_sphere_add(location=(0, 0, 0))
 obj = bpy.context.object
 
@@ -9,3 +20,9 @@ modifier.render_levels = 2
 mesh = obj.data
 for p in mesh.polygons:
     p.use_smooth = True
+    
+# quantum_phase_bloch.py
+import numpy as np
+
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
+#from qiskit.tools.visualization import plot_bloch_vector
