b4e5f7c1f48e62708ba6fedca1aa0e48830f6804
Huang Junye <h.jun.ye@gmail.com>
2019-10-07 08:56:10 +0800

    add smooth sphere

---
+++ blender/bloch_sphere.py
@@ -0,0 +1,11 @@
+import bpy
+bpy.ops.mesh.primitive_ico_sphere_add(location=(0, 0, 0))
+obj = bpy.context.object
+
+modifier = obj.modifiers.new('Subsurf', 'SUBSURF')
+modifier.levels = 2
+modifier.render_levels = 2
+
+mesh = obj.data
+for p in mesh.polygons:
+    p.use_smooth = True
